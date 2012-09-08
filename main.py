722c07f0d0f011e25154798261045c3bb216575e
Cristian Rasch <cristianrasch@gmail.com>
2012-09-08 20:38:14 -0300

    Pantalla de inicio

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# -*- coding: UTF-8 -*-
+
+from ui.inicio import Inicio
+
+if __name__ == "__main__":
+    app = Inicio()
+    app.run()
---
+++ ui/inicio.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+
+import gtk
+import os
+
+class Inicio:
+    LEDS_HORIZONTALES = 16
+    LEDS_VERTICALES = 4
+
+    def __init__(self):
+        self.build_ui_from_xml()
+        self.set_default_values()
+
+    def run(self):
+        gtk.main()
+
+    def on_inicio_delete_event(self, widget, data=None):
+        gtk.main_quit()
+        return False
+
+    def build_ui_from_xml(self):
+        self.builder = gtk.Builder()
+        self.builder.add_from_file(os.path.abspath('ui/cartel-led-inicio.glade'))
+        self.leds_horizontales = self.builder.get_object('leds_horizontales')
+        self.leds_verticales = self.builder.get_object('leds_verticales')
+        self.builder.connect_signals(self)
+
+    def set_default_values(self):
+        self.leds_horizontales.set_value(self.LEDS_HORIZONTALES)
+        self.leds_verticales.set_value(self.LEDS_VERTICALES)
