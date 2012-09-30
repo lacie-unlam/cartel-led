58b42f7582ae31fe4635dbd215861e86d11ec79b
Cristian Rasch <cristianrasch@gmail.com>
2012-09-30 20:41:09 -0300

    Cerrar conn serie en el destructor

--- lib/comm.py
+++ lib/comm.py
@@ -9,7 +9,7 @@ class Serializer:
     
     def __init__(self, matriz):
         self.matriz = matriz
-        self.ser = serial.Serial(device, baudrate=19200)
+        self.ser = serial.Serial(self.device, baudrate=19200)
 
     def write(self):
         from ui.widgets import ModuloLeds
@@ -36,7 +36,6 @@ class Serializer:
             time.sleep(0.01)
         self.ser.write('zm\r')
         print 'zm'
-        self.ser.close()
 
     def write_bytes(self, bytes, r):
         for bits in reversed(bytes):
@@ -44,3 +43,6 @@ class Serializer:
             data = hex(int(hexa, 2))[2:]
             print 'z%s: %s' % (r, data.rjust(4, '0'))
             self.ser.write('z%s%s\r' % (r, data.rjust(4, '0')))
+
+    def __del__(self):
+        self.ser.close()
